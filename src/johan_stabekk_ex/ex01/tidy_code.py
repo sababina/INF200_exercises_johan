8410cd52bee23c828e744c4d129444e69639e81e
Johan Stabekk <johan.stabekk@nmbu.no>
2019-09-16 13:54:35 +0200

    Task D: Commented what the different functions does.

--- src/johan_stabekk_ex/ex01/tidy_code.py
+++ src/johan_stabekk_ex/ex01/tidy_code.py
@@ -4,11 +4,17 @@ __author__ = 'Johan Stabekk'
 __email__ = 'johan.stabekk@nmbu.no'
 
 """
-
+This code is a guessing game where you have to guess a number between 2 and 12. 
+You get points according to how many times you try.
+You have three attempts.
 """
 
 
 def get_valid_input():
+    """
+    This function gets a integer input from the player.
+    :return: The input
+    """
     count = 0
     while count < 1:
         count = int(input('Your guess between 2 and 12: '))
@@ -16,10 +22,20 @@ def get_valid_input():
 
 
 def roll_dice():
+    """
+    Simulates the rolling of who dices.
+    :return: The sum of the two dices.
+    """
     return randint(1, 6) + randint(1, 6)
 
 
 def is_answer_correct(correct, guess):
+    """
+    This function checks if the players answer is correct
+    :param correct: The correct answer
+    :param guess: The players guess
+    :return: Returns if the answer is true or false
+    """
     return correct == guess
 
 

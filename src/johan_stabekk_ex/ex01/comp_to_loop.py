3013f980071385c66cd94944f7d7566ca50f4f60
Johan Stabekk <johan.stabekk@nmbu.no>
2019-09-16 12:58:33 +0200

    Task B: Converted the squares_by_comp to a for loop squares_by_loop.

---
+++ src/johan_stabekk_ex/ex01/comp_to_loop.py
@@ -0,0 +1,28 @@
+
+
+"""
+Solution to task B on exercise 01 in INF200
+"""
+
+
+def squares_by_comp(n):
+    return [k**2 for k in range(n) if k % 3 == 1]
+
+
+def squares_by_loop(n):
+    squares = []
+    for k in range(n):
+        if k % 3 == 1:
+            squares.append(k**2)
+    return squares
+
+
+if __name__ == '__main__':
+
+    how_many_squares = int(input('How many squares do you want?'))
+
+    if squares_by_comp(how_many_squares) != squares_by_loop(how_many_squares):
+        print('ERROR!')
+    else:
+        print('Success')
+

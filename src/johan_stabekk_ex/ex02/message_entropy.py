d70e0663798c60a98de47174b1e6ac1d8223b9c8
Johan Stabekk <johan.stabekk@nmbu.no>
2019-09-25 20:21:47 +0200

    Finished the doc-string of message_entropy and made sure it follow PEP-8

--- src/johan_stabekk_ex/ex02/message_entropy.py
+++ src/johan_stabekk_ex/ex02/message_entropy.py
@@ -10,8 +10,8 @@ def letter_freq(txt):
     """
     This code counts the frequency of how many times a character is used.
     :param txt: input for the user of the code.
-    :return: Returns a dictionary where the frequency of characters in txt is placed, with the character as a key and
-             the number of times as a value
+    :return: Returns a dictionary where the frequency of characters in txt is placed,
+            with the character as a key and the number of times as a value
     """
     freq = {}
     txt_lowered = txt.lower()
@@ -26,13 +26,14 @@ def letter_freq(txt):
 
 
 def entropy(message):
-    """
+    """ Calculates the entropy of a message according to information
+    theory.
 
     Parameters
     ----------
-    message
+    message - A message that the user wants to find the entropy of.
 
-    Returns
+    Returns - The entropy value of the message.
     -------
 
     """
@@ -41,10 +42,8 @@ def entropy(message):
     h = 0
     for n_i in message.values():
         p_i = n_i / sum(message.values())
-        if p_i == 0:
-            h += 0
-        else:
-            h += -p_i * (np.log(p_i))
+        h += -p_i * (np.log(p_i))
+
     return h / np.log(2)
 
 

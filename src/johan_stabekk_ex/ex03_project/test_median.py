a8e1e75aa099e73251352b1d6bc6892ac205cd01
Johan Stabekk <johan.stabekk@nmbu.no>
2019-09-30 13:07:03 +0200

    Created the exercise files and copied the tasks into them.

---
+++ src/johan_stabekk_ex/ex03_project/test_median.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+
+__author__ = ''
+__email__ = '@nmbu.no'
+
+
+def median(data):
+    """
+    Returns median of data.
+
+    :param data: An iterable of containing numbers
+    :return: Median of data
+    """
+
+    sdata = sorted(data)
+    n = len(sdata)
+    return (sdata[n//2] if n % 2 == 1
+            else 0.5 * (sdata[n//2 - 1] + sdata[n//2]))
---
+++ src/johan_stabekk_ex/ex03_project/test_sorting.py
@@ -0,0 +1,69 @@
+# -*- coding: utf-8 -*-
+
+__author__ = ''
+__email__ = '@nmbu.no'
+
+
+def test_empty():
+    """Test that the sorting function works for empty list"""
+    pass
+
+
+def test_single():
+    """Test that the sorting function works for single-element list"""
+    pass
+
+
+def test_sorted_is_not_original():
+    """
+    Test that the sorting function returns a new object.
+
+    Consider
+
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+
+    Now sorted_data shall be a different object than data,
+    not just another name for the same object.
+    """
+    pass
+
+
+def test_original_unchanged():
+    """
+    Test that sorting leaves the original data unchanged.
+
+    Consider
+
+    data = [3, 2, 1]
+    sorted_data = bubble_sort(data)
+
+    Now data shall still contain [3, 2, 1].
+    """
+    pass
+
+
+def test_sort_sorted():
+    """Test that sorting works on sorted data."""
+    pass
+
+
+def test_sort_reversed():
+    """Test that sorting works on reverse-sorted data."""
+    pass
+
+
+def test_sort_all_equal():
+    """Test that sorting handles data with identical elements."""
+    pass
+
+
+def test_sorting():
+    """
+    Test sorting for various test cases.
+
+    This test case should test sorting of a range of data sets and
+    ensure that they are sorted correctly. These could be lists of
+    numbers of different length or lists of strings.
+    """
+    pass

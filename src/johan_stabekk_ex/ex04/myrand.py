52fae2a55e85f9d343aff5688eeec0b266cc5201
Johan Stabekk <johan.stabekk@nmbu.no>
2019-10-18 15:06:07 +0200

    Did task a, made the two different classes and generated some numbers

---
+++ src/johan_stabekk_ex/ex04/myrand.py
@@ -0,0 +1,48 @@
+# -*- coding: utf-8 -*-
+
+
+__author__ = 'Johan Stabekk'
+__email__ = 'johan.stabekk@nmbu.no'
+
+
+class LCGrand:
+    a = 7**5
+    m = 2**32-1
+
+    def __init__(self, seed=None):
+        self.previous = seed
+
+    def rand(self):
+        self.previous = LCGrand.a * self.previous % LCGrand.m
+
+        return self.previous
+
+
+class ListRand:
+
+    def __init__(self, list_of_numbers):
+        self.list_nr = list_of_numbers.copy()
+        self.next = 0
+
+    def rand(self):
+
+        number = self.list_nr[self.next]
+
+        if self.next > len(self.list_nr):
+            raise RuntimeError
+
+        self.next += 1
+
+        return number
+
+
+if __name__ == '__main__':
+
+    list_nr = ListRand([1, 5, 1, 2, 3, 4])
+    seed = LCGrand(256)
+
+    for i in range(6):
+        print(f'Random number from LCGrand {seed.rand()} and i*th number '
+              f'{list_nr.rand()}')
+
+

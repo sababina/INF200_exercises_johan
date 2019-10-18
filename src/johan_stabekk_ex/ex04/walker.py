8558250f8f24809b69f2deec528cc5d9e717faeb
Johan Stabekk <johan.stabekk@nmbu.no>
2019-10-18 15:04:52 +0200

    made the outlines for for the walker task.

---
+++ src/johan_stabekk_ex/ex04/walker.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+
+__author__ = 'Johan Stabekk'
+__email__ = 'johan.stabekk@nmbu.no'
+
+
+class Walker:
+    def __init__(self,start_pos, home):
+        pass
+
+    def move(self):
+        pass
+
+    def is_at_home(self):
+        pass
+
+    def get_position(self):
+        pass
+
+    def get_steps(self):
+        pass

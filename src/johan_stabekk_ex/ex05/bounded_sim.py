a92b5fba5763bfb80a670988043f77fe0c473582
Johan Stabekk <johan.stabekk@nmbu.no>
2019-10-25 14:41:38 +0200

    Tried finishing the bounded_sim, have some trouble with same results

--- src/johan_stabekk_ex/ex05/bounded_sim.py
+++ src/johan_stabekk_ex/ex05/bounded_sim.py
@@ -1,17 +1,61 @@
 # -*- coding: utf-8 -*-
 
-from walker_sim import Walker
-from walker_sim import Simulation
+from walker_sim import Walker, Simulation
 
 __author__ = 'Johan Stabekk'
 __email__ = 'johan.stabekk@nmbu.no'
 
 
-class BoundedWalker:
+class BoundedWalker(Walker):
     def __init__(self, start, home, left_limit, right_limit):
-        pass
+        # if left_limit > start:
+        #    raise ValueError('Start position has to be inside the boundaries')
+        # elif right_limit < home:
+        #     raise ValueError('Home position has to be inside the boundaries')
+
+        super().__init__(start, home)
+        self.left = left_limit
+        self.right = right_limit
+
+    def move(self):
+        super().move()
+
+        if self.position > self.right:
+            self.position = self.right
+        elif self.position < self.left:
+            self.position = self.left
 
 
-class BoundedSimulation:
+class BoundedSimulation(Simulation):
     def __init__(self, start, home, seed, left_limit, right_limit):
-        pass
+
+        super().__init__(start, home, seed)
+        self.right = right_limit
+        self.left = left_limit
+
+    def walk(self):
+        walker = BoundedWalker(self.start, self.home, self.left, self.right)
+
+        stride = 0
+        while not walker.is_at_home():
+            walker.move()
+            stride += 1
+
+        return stride
+
+
+if __name__ == '__main__':
+    boundaries = [0, -10, -100, -1000, -10000]
+
+    for boundary in boundaries:
+        steps = BoundedSimulation(0, 20, 12345, boundary, 20).run_simulation(20)
+        print(f'Left boundary {boundary:8d}: {steps}')
+
+
+
+
+
+
+
+
+

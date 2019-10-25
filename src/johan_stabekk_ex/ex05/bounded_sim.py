2b6c04a0c6ec3a23f306cd4ed2c1c0a279adb233
Johan Stabekk <johan.stabekk@nmbu.no>
2019-10-25 11:04:56 +0200

    Made the class structure for task: Bounded_sim and myrand.

--- src/johan_stabekk_ex/ex05/bounded_sim.py
+++ src/johan_stabekk_ex/ex05/bounded_sim.py
@@ -1,4 +1,17 @@
 # -*- coding: utf-8 -*-
 
+from walker_sim import Walker
+from walker_sim import Simulation
+
 __author__ = 'Johan Stabekk'
 __email__ = 'johan.stabekk@nmbu.no'
+
+
+class BoundedWalker:
+    def __init__(self, start, home, left_limit, right_limit):
+        pass
+
+
+class BoundedSimulation:
+    def __init__(self, start, home, seed, left_limit, right_limit):
+        pass
--- src/johan_stabekk_ex/ex05/myrand.py
+++ src/johan_stabekk_ex/ex05/myrand.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 
+import random
+
 __author__ = 'Johan Stabekk'
 __email__ = 'johan.stabekk@nmbu.no'
+
+
+class RandIter:
+    def __init__(self, random_number_generator, length):
+        pass
+
+    def __iter__(self):
+        pass
+    
+    def __next__(self):
+        pass
+

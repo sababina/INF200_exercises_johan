606a1f9952a67561542c55cbcbf809e4ccf4710e
Johan Stabekk <johan.stabekk@nmbu.no>
2019-10-29 08:28:31 +0100

    Added docstring

--- src/johan_stabekk_ex/ex05/myrand.py
+++ src/johan_stabekk_ex/ex05/myrand.py
@@ -9,9 +9,25 @@ class LCGRand:
     congruence_class = 2 ** 31 - 1
 
     def __init__(self, seed):
+        """
+        Initialise a linear congruence random number generator
+
+        Arguments
+        ---------
+        seed : int
+            The initial seed for the generator
+        """
         self._hidden_state = seed
 
     def rand(self):
+        """
+        Generates a single random number.
+
+        Returns
+        -------
+        int
+            A random integer
+        """
         self._hidden_state *= self.slope
         self._hidden_state %= self.congruence_class
 
@@ -21,17 +37,47 @@ class LCGRand:
         return RandIter(self, length)
 
     def infinite_random_sequence(self):
+        """
+        Generate an infinite sequence of random numbers.
+
+        Yields
+        ------
+        int
+            A random number.
+        """
         while True:
             yield self.rand()
 
 
 class RandIter:
     def __init__(self, random_number_generator, length):
+        """
+
+        Arguments
+        ---------
+        random_number_generator :
+            A random number generator with a ``rand`` method that
+            takes no arguments and returns a random number.
+        length : int
+            The number of random numbers to generate
+        """
         self.generator = random_number_generator
         self.length = length
         self.num_generated_numbers = None
 
     def __iter__(self):
+        """
+        Initialise the iterator.
+
+        Returns
+        -------
+        self : RandIter
+
+        Raises
+        ------
+        RuntimeError
+            If iter is called twice on the same RandIter object.
+        """
         if self.num_generated_numbers is not None:
             raise RuntimeError('Can not iterate through twice')
 
@@ -39,6 +85,21 @@ class RandIter:
         return self
 
     def __next__(self):
+        """
+        Generate the next random number.
+
+        Returns
+        -------
+        int
+            A random number.
+
+        Raises
+        ------
+        RuntimeError
+            If the ``__next__`` method is called before ``__iter__``.
+        StopIteration
+            If ``self.length`` random numbers are generated.
+        """
         if self.num_generated_numbers is None:
             raise RuntimeError(
                 'Can not call ``next`` before the the iteration is started'
